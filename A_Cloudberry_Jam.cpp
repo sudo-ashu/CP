cce736a56692698775cd84357b602f36965f0c88
Ashutosh Rathore <rathoreashutosh3006@gmail.com>
2025-06-15 23:10:43 +0530

    adding 40th question

---
+++ A_Cloudberry_Jam.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+// 3 jars of 3kg (2,2,2)
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t; cin>> t;
+    
+    while(t--) {
+        ll n; cin >> n;
+        cout << 2*n << endl;
+    }
+
+    return 0;
+}
